54e5d80a60d66cd24fee39c21a358ddf1ad55206
bkroggel <b.kroggel@yahoo.de>
2017-07-20 19:15:40 +0200

    initial implementation of sticky nav elements on mobile_responsive view

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
-console.log("ready");
+  // blog-dropdown
   $('#blog-nav-dropdown').click(function() {
-    console.log("läuft")
     if ($('.bk-blog-sidebar').hasClass('active')) {
       $('.bk-blog-sidebar, #blog-nav-dropdown').removeClass('active');
       $(document.body).css('overflow', 'auto');
@@ -11,5 +10,35 @@ console.log("ready");
       $('.bk-blog-sidebar').scrollTop(0);
     }
   });
+
+
+  // sticky navbar on mobile
+  if($(window).width() <= 991) {
+    var sticky = new Waypoint.Sticky({
+      element: $('.bk-blog-nav')[0]
+    })
+
+
+    var navHeight = $('.bk-blog-nav').outerHeight(true);
+    var bk_wrapper = '<div class="bk-blog__navigation-wrapper" />'
+    var secondHeight = $('.bk-blog__navigation').height();
+
+    $('.bk-blog__navigation').wrap(bk_wrapper);
+    $('.bk-blog__navigation-wrapper').height(secondHeight);
+
+    $('.bk-blog__navigation').waypoint({
+      handler: function(direction) {
+        console.log("test");
+        if (direction === 'down') {
+          $('.bk-blog__navigation').addClass('stuck');
+        } else {
+          $('.bk-blog__navigation').removeClass('stuck');
+        }
+      },
+      offset: navHeight
+    });
+
+  }
+
 
 });

f7bd4a7104f02331dcb35d8f4fbc9732c86a7b42
bkroggel <b.kroggel@yahoo.de>
2018-04-16 22:51:36 +0200

    initiated compress function, distributed small part of js to single inline object since it is just needed on tags page

--- assets/js/main.js
+++ assets/js/main.js
@@ -70,32 +70,6 @@ $('#storewindow__img').on('init', function(){
   speed: 1200
 });
 
-/* Tags */
-function tag_alphabet(){
-  var alphabet = $('.tags__alphabet ul li');
-  var marker = $('.tags__element h3')
-  marker.each(function(){
-    var letter = $(this).text();
-    if($(window).width() < 767) {
-      //removed href so scroll_alphabet will work
-      $('.tags__alphabet ul li:contains("'+letter+'")').wrapInner( '<a></a>');
-    }else{
-      $('.tags__alphabet ul li:contains("'+letter+'")').wrapInner( '<a href="#'+letter+'"></a>');
-    }
-
-  })
-}
-tag_alphabet();
-
-$('.tags__alphabet ul li a').click(function(e) {
-  if($(window).width() < 767) {
-    console.log('jep smaller')
-    var letter = $(this).text();
-    $('html, body').scrollTop($('#'+letter).offset().top - 160);
-    e.preventDefault();
-  }
-})
-
 /* Sidebar + Nav */
 function set_waypoint(){
   var sticky = new Waypoint.Sticky({
